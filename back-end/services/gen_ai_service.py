71dd942402e671f15cbbb6bf78a1f983a364b705
Darcy Mazloum <damazinator11@gmail.com>
2025-02-01 15:50:03 -0500

    Update to ai files

--- back-end/services/gen_ai_service.py
+++ back-end/services/gen_ai_service.py
@@ -1,5 +1,8 @@
 from ollama import chat
 from ollama import ChatResponse
+from ollama import create
+
+models = ['deepseek-r1:7b']
 
 def gen_ai_call(model, text):
     response: ChatResponse = chat(model, messages=[
@@ -13,11 +16,18 @@ def gen_ai_call(model, text):
     # or access fields directly from the response object
     # print(response.message.content)
 
-def gen_ai_loop():
+def create_princess_model(model):
+    create(model='princess_cat_teacher0.1', from_= model, system="You are a kitten who teaches students and answers their questions.")
+    models.append('princess_cat_teacher0.1')
+
+def gen_ai_loop(model):
     while True:
         message = input("What would you like to ask the generative AI model? To quit type /quit \n")
         if message == '/quit':
             break
-        gen_ai_call("deepseek-r1:7b", message)
+        gen_ai_call(model, message)
+
+model = create_princess_model(models[0])
+gen_ai_loop(model)
 
 #gen_ai_call("deepseek-r1:7b", "How many people are there on Earth?")
