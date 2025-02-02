1d8cf5786607368163c140a7d9430b185c97ec5a
Darcy Mazloum <damazinator11@gmail.com>
2025-02-01 22:20:21 -0500

    Working on sliders

--- front-end/javascript/js_index.js
+++ front-end/javascript/js_index.js
@@ -8,5 +8,29 @@ when you press search, get all the infromation and put it like in an array or sm
 */
 
 /*
-<input type="range" min="1" max="100" value="50"></input>
+1. auto hide the bars  X
+2. when any of the checkmarks are pressed then the publish date bar appears
+3. when video is checked then length appears
+4. display the value on the slider on the side
 */
+
+const pub_slider = document.getElementById("publish_date")
+const vid_slider = document.getElementById("video_length")
+
+vid_slider.style.display = 'none'
+
+const vid_check = document.getElementById("videos")
+const exams_check = document.getElementById("exams")
+const lect_note_check = document.getElementById("lecture_notes")
+
+
+vid_check.addEventListener('change', function()
+{
+    if (this.checked) {
+        vid_slider.style.display = 'initial'
+    }
+    else {
+        vid_slider.style.display = 'none'
+    }
+})
+
