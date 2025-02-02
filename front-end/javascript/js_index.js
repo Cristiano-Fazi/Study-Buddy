e7d100324c58bb293f6ddd36dc28d2a2988b4bda
Darcy Mazloum <damazinator11@gmail.com>
2025-02-01 22:56:18 -0500

    Update js_index.js

--- front-end/javascript/js_index.js
+++ front-end/javascript/js_index.js
@@ -7,30 +7,40 @@ do it for each checkbox
 when you press search, get all the infromation and put it like in an array or smt 
 */
 
-/*
-1. auto hide the bars  X
-2. when any of the checkmarks are pressed then the publish date bar appears
-3. when video is checked then length appears
-4. display the value on the slider on the side
-*/
-
 const pub_slider = document.getElementById("publish_date")
 const vid_slider = document.getElementById("video_length")
 
 vid_slider.style.display = 'none'
+pub_slider.style.display = 'none'
 
 const vid_check = document.getElementById("videos")
 const exams_check = document.getElementById("exams")
 const lect_note_check = document.getElementById("lecture_notes")
 
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
 
 vid_check.addEventListener('change', function()
 {
     if (this.checked) {
-        vid_slider.style.display = 'initial'
+        vid_slider.style.display = 'block' // I think block is how to do it
     }
     else {
         vid_slider.style.display = 'none'
     }
+    
 })
 
+function updateElementVisibility()
+{
+    const isAnyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked)
+    if (isAnyChecked) {
+        pub_slider.style.display = "block";
+    } else {
+        pub_slider.style.display = "none";
+    }
+}
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("change", updateElementVisibility);
+});
